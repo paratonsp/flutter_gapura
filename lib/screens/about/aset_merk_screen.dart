e3a36a76de80326f99dd52a7128bcf63fa8178c5
paratonsp <paratonsp@gmail.com>
2022-05-30 17:15:11 +0700

    setup

--- lib/screens/about/aset_merk_screen.dart
+++ lib/screens/about/aset_merk_screen.dart
@@ -7,7 +7,6 @@ import 'package:flutter/material.dart';
 import 'package:fluttertoast/fluttertoast.dart';
 
 import 'package:gapura/constants.dart';
-import 'package:gapura/responsive.dart';
 import 'package:gapura/screens/components/header.dart';
 import 'package:http/http.dart' as http;
 import 'package:flutter_dotenv/flutter_dotenv.dart';
